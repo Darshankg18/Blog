8341f96e53a6905a7ddf6ca10715fd19c4f72b80
Darshan K G <kgdarshan2005@gmail.com>
2026-03-31 22:12:11 +0530

    Blogs and Search Features

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,17 +1,25 @@
 
 from django.shortcuts import render
+from assignments.models import About
 from blogs.models import Blog,category
 
 def home(request):
-    
+    #categories=category.objects.all()
     featured_posts=Blog.objects.filter(is_featured=True,status='Published').order_by('-updated_at')
     #featured_postss=Blog.objects.filter(is_featured=True)
     posts=Blog.objects.filter(status='Published',is_featured=False)
+    
+    #Fetch about us
+    try:
+        about=About.objects.get()
+    except:
+        about=None
     context={
-        
+        #'categories':categories
         'featured_posts':featured_posts,
        # 'featured_postss':featured_postss,
        'posts':posts,
+       'about':about,
 
     }
     return render(request,'home.html',context)
--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,6 +1,10 @@
+from assignments.models import SocialLink
 from blogs.models import category
 
 
 def get_categories(request):
     categories=category.objects.all()
     return dict(categories=categories)
+def get_social_links(request):
+    social_links=SocialLink.objects.all()
+    return dict(social_links=social_links)
