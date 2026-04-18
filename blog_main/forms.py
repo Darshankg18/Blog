1febd9a0e9b760cf89771471881d38537fd03137
Darshan K G <kgdarshan2005@gmail.com>
2026-04-18 22:42:12 +0530

    Registration and Login Functionality

---
+++ blog_main/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+class RegistrationForm(UserCreationForm):
+    # class Meta:
+    #     model=User
+    #     fields=('email','username','password1','password2')
+
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password1', 'password2')
